3ff5816f4f926f134a3d472cad1398d0fd0ad980
GalychIvan <galych.ivan@ukr.net>
2023-03-03 10:32:08 +0200

    adit shedule

--- resources/views/edumate/schedule.blade.php
+++ resources/views/edumate/schedule.blade.php
@@ -7,9 +7,12 @@
 @endsection
 
 @section('content')
+
+    @php($link = 'documents/schedule/2022-2023-II/')
+
     <section class="container">
-        <div class="container ml-15">
-            <div class="row mt-20">
+        <div class="container ml-10">
+            <div class="row ">
                 <div class="col-lg-12">
                     <div class="section-title-2 mt-30">
                         <h2 class="title">208 Агроінженерія</h2>
@@ -20,27 +23,27 @@
             <div class="">
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/208-1.pdf') }}" target="_blank">1 курс (11М, 12М, 13М)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/208/208-1.pdf') }}" target="_blank">1 курс (11М, 12М, 13М)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/208-2.pdf') }}" target="_blank">2 курс (21М, 22М, 23М, 25М, 26Мпр)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/208/208-2.pdf') }}" target="_blank">2 курс (21М, 22М, 23М, 25М, 26Мпр)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/208-3.pdf') }}" target="_blank">3 курс, (31М, 32М, 33М, 34Мпр, 35Мпр, 36Мпр, 38М, 38Мпр)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/208/208-3.pdf') }}" target="_blank">3 курс, (31М, 32М, 33М, 34Мпр, 35Мпр, 36Мпр, 38М, 38Мпр)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/208-4.pdf') }}" target="_blank">4 курс, (41М, 42М, 43М, 44Мпр, 45Мпр, 46Мпр, 48М, 48Мпр)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/208/208-4.pdf') }}" target="_blank">4 курс, (41М, 42М, 43М, 44Мпр, 45Мпр, 46Мпр, 48М, 48Мпр)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/208-5-ОНП.pdf') }}" target="_blank">1 курс ОНП (51М)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/208/208-5-ОНП.pdf') }}" target="_blank">1 курс магістерський ОНП (51М)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/208-5-ОПП.pdf') }}" target="_blank">1 курс ОПП (52М, 53М, 54М, 55М)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/208/208-5-ОПП.pdf') }}" target="_blank">1 курс магістерський ОПП (52М, 53М, 54М, 55М)</a></h3>
                 </div>
             </div>
 
@@ -55,23 +58,23 @@
             <div class="">
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/274-1.pdf') }}" target="_blank">1 курс (14АТ)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/274/274-1.pdf') }}" target="_blank">1 курс (14АТ)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/274-2.pdf') }}" target="_blank">2 курс (24АТ, 24АТпр)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/274/274-2.pdf') }}" target="_blank">2 курс (24АТ, 24АТпр)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/274-3.pdf') }}" target="_blank">3 курс, (37АТ)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/274/274-3.pdf') }}" target="_blank">3 курс, (37АТ)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/274-4.pdf') }}" target="_blank">4 курс, (47АТ, 47ТАпр)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/274/274-4.pdf') }}" target="_blank">4 курс, (47АТ, 47ТАпр)</a></h3>
                 </div>
                 <div class="single-notice">
                     <span class="number">-</span>
-                    <h3 class="notice-title"><a href="{{ asset('/pdf/schedule/274-5.pdf') }}" target="_blank">1 курс ОПП (56АТ)</a></h3>
+                    <h3 class="notice-title"><a href="{{ asset($link.'/274/274-5.pdf') }}" target="_blank">1 курс магістерський ОПП (56АТ)</a></h3>
                 </div>
             </div>
 
